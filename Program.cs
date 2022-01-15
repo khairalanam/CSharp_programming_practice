bb25169c26a2db7f468c36e92d39323995ed05b8
Khair Al Anam <anaambasheer@gmail.com>
2022-01-15 09:00:49 +0530

    first cs program

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace FirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+            Console.ReadLine();
+        }
+    }
+}
